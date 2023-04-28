264fd318492b856ad68139f4d0c472a827f183c1
KolupaevAlexander <113934423+KolupaevAlexander@users.noreply.github.com>
2023-04-28 10:00:37 +0500

    Add files via upload

---
+++ Практика 18/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Практика_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short m;
+            Console.Write("Введите число: ");
+            while (!Int16.TryParse(Console.ReadLine(), out m))
+            {
+                Console.Write("Неверный формат ввода. Повторите ввод: ");
+            }
+
+            string path = "sum.txt";
+            short[] data;
+            if (File.Exists(path))
+            {
+                data = File.ReadAllText(path)
+                           .Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                           .Select(x => Int16.Parse(x))
+                           .ToArray();
+
+                var list = Count(data);
+
+                foreach (var item in list) Console.WriteLine(item); 
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static List<int> Count(short[] data)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < data.Length*data.Length-1; i++) 
+            {
+                list.Add(SetSum(data, GenerateCode(i, data.Length)));
+            }
+
+            return list;
+        }
+        static int SetSum(short[] set, string code)
+        {
+            int sum = 0;
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (code[i] == '1')
+                    sum += set[i];
+            }
+            return sum;
+        }
+        static string GenerateCode(int number, int length)
+        {
+            string code = Convert.ToString(number, 2);
+                while (code.Length != length)
+                {
+                    code = '0' + code;
+                }
+            return code;
+        }
+
+    }
+}
